2360bb10fe6d2a7c509275250d5c2baa11e13a16
Julien YLLAN <git@yllan.fr>
2019-10-01 23:44:37 +0200

    [WIP] Try to test dom node after async mocked call

--- src/components/City.js
+++ src/components/City.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from "react-bootstrap/Card";
+import axios from "axios";
 import PropTypes from 'prop-types'
 import '../style/weather/style.scss'
 
@@ -15,37 +16,57 @@ class City extends React.Component {
         };
     }
 
-    componentDidMount() {
+    async componentDidMount() {
         if (this.props.showDetails) {
-            fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=d772bba149811a24fc8a81b2a52d6b3e&lang=fr&units=metric&id=${this.props.openweatherCityId}`)
-                .then(res => res.json())
-                .then(
-                    (result) => {
-                          this.setState({
-                            isLoaded: true,
-                            infos: result,
-                          });
-                    },
-                    // Note: it's important to handle errors here
-                    // instead of a catch() block so that we don't swallow
-                    // exceptions from actual bugs in components.
-                    (error) => {
-                          this.setState({
-                            isLoaded: true,
-                            error,
-                          });
+            try {
+                // let result = await this.getData();
+                let result = await axios.get('http://api.openweathermap.org/data/2.5/weather', {
+                    params: {
+                        APPID: 'd772bba149811a24fc8a81b2a52d6b3e',
+                        units: 'metric',
+                        id: this.props.openweatherCityId,
+                        lang: 'fr',
                     }
-                )
+                });
+
+                if (result) {
+                    this.setState({
+                        isLoaded: true,
+                        infos: result.data,
+                    });
+                }
+            } catch (e) {
+                this.setState({
+                    isLoaded: true,
+                    error: e.message,
+                  });
+            }
         }
     }
+
+    // async getData() {
+    //     return await axios.get('http://api.openweathermap.org/data/2.5/weather', {
+    //         params: {
+    //             APPID: 'd772bba149811a24fc8a81b2a52d6b3e',
+    //             units: 'metric',
+    //             id: this.props.openweatherCityId,
+    //             lang: 'fr',
+    //         }
+    //     });
+    // }
 
     fullView () {
         if (this.state.isLoaded) {
             if (this.state.infos.id) {
+                console.log(this.state.infos.name);
                 return (
                     <Card>
                         {/* <Card.Header>{this.props.openweatherCityId}</Card.Header> */}
-                        <Card.Header>{this.state.infos.name}</Card.Header>
+                        <Card.Header>
+                            <div className="city__name">
+                                {this.state.infos.name}
+                            </div>
+                        </Card.Header>
                         <Card.Body>
                             <div className="city__weather__type">
                                 {
@@ -90,14 +111,19 @@ class City extends React.Component {
                 return (
                     <Card>
                         <Card.Header>Ville : {this.props.openweatherCityId}</Card.Header>
-                        <Card.Body>ERREUR</Card.Body>
+                        <Card.Body>ERREUR: {this.state.error}</Card.Body>
                     </Card>
                 )
             }
         } else {
+            console.log('City - 110 - ');
             return (
                 <Card>
-                    <Card.Body>Chargement...</Card.Body>
+                    <Card.Body>
+                        <div className="city__loading">
+                            Chargement...
+                        </div>
+                    </Card.Body>
                 </Card>
             )
         }
@@ -144,7 +170,7 @@ City.propTypes = {
     onRemove: PropTypes.func.isRequired,
     active: PropTypes.bool.isRequired,
     showDetails: PropTypes.bool,
-    openweatherCityId: PropTypes.string.isRequired
+    openweatherCityId: PropTypes.string.isRequired,
 }
 
-export default City
+export default City;
--- src/components/City.test.js
+++ src/components/City.test.js
@@ -1,38 +1,153 @@
 import React from 'react';
-import { shallow, configure } from 'enzyme';
+import _ from 'lodash';
+// import sinon from 'sinon';
+import {
+    shallow,
+    configure
+} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import City from './City';
-
+import axios from "axios";
 
 //Use array destructurig to create mock functions.
 let [onDisable, onRemove] = new Array(2).fill(jest.fn());
 
-function shallowSetup() {
+function shallowSetup(customProps = {}) {
     // Sample props to pass to our shallow render
-    const props = {
+    const defaultProps = {
         id: "0",
-        openweatherCityId: "2992166",
+        openweatherCityId: "2992166", // Montpellier
         active: true,
         showDetails: false,
         onDisable: onDisable,
         onRemove: onRemove
     }
 
-    configure({ adapter: new Adapter() });
+    const props = _.merge(defaultProps, customProps);
+    configure({
+        adapter: new Adapter()
+    });
 
     // wrapper instance around rendered output
-    const enzymeWrapper = shallow(<City {...props} />);
+    const enzymeWrapper = shallow( < City {
+            ...props
+        }
+        />);
 
-    return {
-        props,
-        enzymeWrapper
-    };
-}
+        return {
+            props,
+            enzymeWrapper
+        };
+    }
 
-describe('Shallow rendered City', () => {
-    it('should render a City without details. Only the ID', () => {
-      // Setup wrapper and assign props.
-      const { enzymeWrapper, props } = shallowSetup();
-      expect(enzymeWrapper.find('.city__openweather-id').text()).toBe(props.openweatherCityId);
+
+    describe('Shallow rendered listing City', () => {
+        //   it('should render a City without details. Only the ID', () => {
+        //     // Setup wrapper and assign props.
+        //     const {
+        //       enzymeWrapper,
+        //       props
+        //     } = shallowSetup();
+
+        //     expect(enzymeWrapper.find('.city__openweather-id').text()).toBe(props.openweatherCityId);
+        // });
+
+        test('should render a City with details. Check city name', async () => {
+
+
+
+            jest.mock('axios');
+
+            const getSpy = await jest.spyOn(axios, 'get');
+
+            const {
+                enzymeWrapper,
+                props
+            } = shallowSetup({
+                showDetails: true
+            });
+
+
+            expect(getSpy).toBeCalled();
+
+            //   mockAxios.get.mockImplementationOnce();
+
+            // axios.get.mockResolvedValue({
+            //     data: {
+            //         "coord": {
+            //             "lon": 3.88,
+            //             "lat": 43.61
+            //         },
+            //         "weather": [{
+            //             "id": 500,
+            //             "main": "Rain",
+            //             "description": "légère pluie",
+            //             "icon": "10n"
+            //         }],
+            //         "base": "stations",
+            //         "main": {
+            //             "temp": 20.38,
+            //             "pressure": 1011,
+            //             "humidity": 88,
+            //             "temp_min": 19,
+            //             "temp_max": 21
+            //         },
+            //         "visibility": 10000,
+            //         "wind": {
+            //             "speed": 5.1,
+            //             "deg": 280
+            //         },
+            //         "clouds": {
+            //             "all": 100
+            //         },
+            //         "dt": 1569959705,
+            //         "sys": {
+            //             "type": 1,
+            //             "id": 6518,
+            //             "message": 0.0063,
+            //             "country": "FR",
+            //             "sunrise": 1569908479,
+            //             "sunset": 1569950825
+            //         },
+            //         "timezone": 7200,
+            //         "id": 2992166,
+            //         "name": "Montpellier",
+            //         "cod": 200
+            //     }
+            // });
+
+            // expect(axios.get).toHaveBeenCalled()
+            // expect(enzymeWrapper.find('.city__loading').text()).toBe('Chargement...');
+            console.log('City.test - 119 - ',enzymeWrapper.debug());
+            expect(enzymeWrapper.find('.city__name').text()).toBe('Montpellier');
+
+
+
+            //   expect(mockAxios.get).toHaveBeenCalledWith(
+            //     "http://api.openweathermap.org/data/2.5/weather", {
+            //         params: {
+            //             APPID: 'd772bba149811a24fc8a81b2a52d6b3e',
+            //             units: 'metric',
+            //             id: props.openweatherCityId,
+            //             lang: 'fr',
+            //         }
+            //     }
+            //   );
+
+            // TO DO : wait for the async call
+            // jest.setTimeout(3000);
+            // expect(enzymeWrapper.find('.city__name').text()).toBe('Montpellier');
+
+
+
+
+
+            // jest.useFakeTimers();
+            // setTimeout(() => {
+            //     // console.log('aaaa', enzymeWrapper.find('.city__name').text());
+            //     // expect(enzymeWrapper.find('.city__name').text()).toBe('Montpellier');
+            // }, 1500);
+            // jest.runAllTimers();
+        });
+
     });
-  });
